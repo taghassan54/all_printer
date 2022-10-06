40007b9f500996c7fa41e78bdc71019d854e8c15
taghassan54 <41596304+taghassan54@users.noreply.github.com>
2022-10-06 10:03:53 +0400

    Update AllPrinterPlugin.kt

--- android/src/main/kotlin/com/example/all_printer/AllPrinterPlugin.kt
+++ android/src/main/kotlin/com/example/all_printer/AllPrinterPlugin.kt
@@ -161,7 +161,7 @@ class AllPrinterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
             printerObject?.printReyBitmap(logoPath)
 
         text += "\n"
-        printerObject?.printRey(text,45)
+        printerObject?.printRey(text,35)
 
 
         return "${getDeviceName()}"
